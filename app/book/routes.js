54c7af3096e7ba78db84e206e8b0b869492c72c8
Joshua <janorris65@gmail.com>
2023-01-10 13:53:00 -0600

    match Manav

---
+++ app/book/routes.js
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import controller from "./controller.js";
+
+const router = Router();
+
+router.post("/seed", (_, res) => {
+  controller
+    .seed()
+    .then(() => {
+      res.json({ message: "Seeded books" });
+    })
+    .catch((err) => {
+      // 500: Internal Server Error
+      res.status(500).json({ message: err.message });
+    });
+});
+
+// GET /api/books
+router.get("/", (_, res) => {
+  controller
+    .index()
+    .then((books) => {
+      res.json(books);
+    })
+    .catch((err) => {
+      // 500: Internal Server Error
+      res.status(500).json({ message: err.message });
+    });
+});
+export default router;
